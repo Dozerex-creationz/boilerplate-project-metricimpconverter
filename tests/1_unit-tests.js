31d2f7dea11bed18f78282105599a83a72bf8f63
Dhakshin Krishna <78342331+Dozerex-creationz@users.noreply.github.com>
2022-01-04 06:33:02 +0000

    up until unit tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,5 +5,65 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 let convertHandler = new ConvertHandler();
 
 suite('Unit Tests', function(){
+//for all valid input read numbericals and display error if wrong
+test('#equal #notEqual #isNotNull',function(){
+  assert.isNotNull(convertHandler.getNum("10mi"));
+});
+test('',function(){
+  assert.isNotNull(convertHandler.getNum("10.09mi"));
+});
+test('',function(){
+  assert.isNotNull(convertHandler.getNum("10/3mi"));
+});
+test('',function(){
+  assert.isNotNull(convertHandler.getNum("1.3/2mi"));
+});
+test('',function(){
+  assert.equal(convertHandler.getNum("1/2/3mi"),"invalid");
+});
+test('',function(){
+  assert.equal(convertHandler.getNum("mi"),1);
+});
+
+//units are perfect or wrong 
+test('#isOk #isNotOk #equal',function(){
+  assert.isOk(convertHandler.getUnit("10mi"));
+});
+test('',function(){
+  assert.equal(convertHandler.getUnit("10.09kgs"),"invalid");
+});
+test('',function(){
+  assert.equal(convertHandler.getReturnUnit("L"),"gal");
+});
+test('',function(){
+  assert.equal(convertHandler.spellOutUnit("kg"),"kilograms");
+});
+//convert unit to correct unit
+test('#equal',function(){
+  assert.equal(convertHandler.getReturnUnit("L"),"gal");
+});
+
+test('',function(){
+
+  assert.equal(convertHandler.getReturnUnit("km"),"mi");
+});
+test('',function(){
+  
+  assert.equal(convertHandler.getReturnUnit("mi"),"km");
+});
+test('',function(){
+  
+  assert.equal(convertHandler.getReturnUnit("lbs"),"kg");
+});
+test('',function(){
+  
+  assert.equal(convertHandler.getReturnUnit("kg"),"lbs");
+});
+test('',function(){
+  
+  assert.equal(convertHandler.getReturnUnit("gal"),"L");
+
+});
+
 
 });
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,19 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-
+test('10L: GET',function(){
+  assert.equal(server,{"initNum":10,"initUnit":"L","returnNum":2.64172,"returnUnit":"gal","string":"10 liters converts to 2.64172 gallons"});
+});
+test('32g: GET',function(){
+assert.equal(,"invalid unit");
+});
+test('3/7.2/4kg: GET',function(){
+assert.equal(,"invalid number");
+});
+test('3/7.2/4kilomegagram: GET',function(){
+assert.equal(,"invalid number and unit");
+});
+test('kg: GET',function(){
+assert.equal(,{"initNum":1,"initUnit":"kg","returnNum":2.20462,"returnUnit":"lbs","string":"1 kilograms converts to 2.20462 pounds"});
+});
 });
